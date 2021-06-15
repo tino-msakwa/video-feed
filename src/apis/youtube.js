223e8ca3f8f420de8a7f453f7c2ede0fe0ea89a5
GC <gc@Tinos-MacBook-Pro.local>
2021-06-15 16:52:20 +1000

    Link API request to search term for app

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,12 @@
-import axios from'axios'
+import axios from "axios";
 const KEY = "IzaSyBRD4BgOUBprJFw9819VIa-0fA991KADv0";
 
 export default axios.create({
-    baseURL: "http://www.googleapis.com /youtube/v3",
-    params : {
-        part: 'snippet';
-        maxResult: 5,
-        type: 'video',
-        key : KEY
-    }
+  baseURL: "http://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
 });
